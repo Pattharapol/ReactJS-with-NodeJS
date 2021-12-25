8e2f04a768edbd21f5a4412a55d7ae6c3847e66a
unknown <tjtik83@gmail.com>
2021-12-25 23:15:37 +0700

    project setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models');
+
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log('Server running on port 3001');
+    });
+});
